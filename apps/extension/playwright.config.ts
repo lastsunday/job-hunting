0457f867a2984c764acf815c5728cda043e73d38
lastsunday <lastsunday@yeah.net>
2025-02-21 18:37:19 +0800

    test(extension): add e2e tests and promote app startup checking(for bridge)

---
+++ apps/extension/playwright.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig, devices } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "e2e",
+
+  // Fail the build on CI if you accidentally left test.only in the source code.
+  forbidOnly: !!process.env.CI,
+
+  // Retry on CI only.
+  retries: process.env.CI ? 2 : 0,
+
+  // Opt out of parallel tests on CI.
+  workers: process.env.CI ? 1 : undefined,
+
+  // Reporter to use
+  reporter: "html",
+
+  use: {
+    // Collect trace when retrying the failed test.
+    trace: "on-first-retry",
+  },
+
+  // Configure projects for major browsers.
+  projects: [
+    {
+      name: "chromium",
+      //channel: "chromium" -> https://github.com/microsoft/playwright/issues/33682
+      use: { ...devices["Desktop Chrome"], channel: "chromium" },
+    },
+  ],
+});
