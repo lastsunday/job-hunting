53d8fc56acf95af185965c55a7d0953ecfd49f4f
lastsunday <lastsunday@yeah.net>
2025-05-20 16:33:48 +0800

    fix(extension): job public service test case date expect error

--- apps/extension/tests/worker/service/jobPublicServiceQueryMinCreateDatetimeGroupByJobId.test.js
+++ apps/extension/tests/worker/service/jobPublicServiceQueryMinCreateDatetimeGroupByJobId.test.js
@@ -16,10 +16,10 @@ test('job public service query min createDatetime group by jobid correct', async
   const item2 = { id: null, jobId: "jobId2", createDatetime: parse("2025-05-19"), updateDatetime: parse("2025-05-19"), sourceType: 0, source: null };
   const item_1 = { id: null, jobId: "jobId1", createDatetime: parse("2025-05-19"), updateDatetime: parse("2025-05-19"), sourceType: 0, source: "lastsunday" };
   const item2_1 = { id: null, jobId: "jobId2", createDatetime: parse("2025-05-17"), updateDatetime: parse("2025-05-17"), sourceType: 0, source: "lastsunday" };
-  const addResult = await SERVICE_INSTANCE._batchAddOrUpdate([item, item2, item_1, item2_1],{overrideCreateDatetime : true, overrideUpdateDatetime : true});
+  const addResult = await SERVICE_INSTANCE._batchAddOrUpdate([item, item2, item_1, item2_1], { overrideCreateDatetime: true, overrideUpdateDatetime: true });
   expect(addResult.length).toBe(4);
   const result = await _queryMinCreateDatetimeGroupByJobId(["jobId1", "jobId2"]);
-  expect(dateToStr(result.filter(item => item.jobId == "jobId1")[0].createDatetime)).toBe(dateToStr(parse("2025-05-18")));
-  expect(dateToStr(result.filter(item => item.jobId == "jobId2")[0].createDatetime)).toBe(dateToStr(parse("2025-05-17")));
+  expect(dateToStr(parse(result.filter(item => item.jobId == "jobId1")[0].createDatetime))).toBe(dateToStr(parse("2025-05-18")));
+  expect(dateToStr(parse(result.filter(item => item.jobId == "jobId2")[0].createDatetime))).toBe(dateToStr(parse("2025-05-17")));
 })
 
