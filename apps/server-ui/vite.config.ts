e1cb2e91b233c5a24c1f25ef5e9d4e17df93e76a
lastsunday <lastsunday@yeah.net>
2025-06-21 13:27:39 +0800

    fix(server-ui): fix vitest error

--- apps/server-ui/vite.config.ts
+++ apps/server-ui/vite.config.ts
@@ -1,6 +1,6 @@
 /// <reference types='vitest' />
 import { defineConfig } from 'vite';
-import reactOxc from '@vitejs/plugin-react-oxc';
+import react from "@vitejs/plugin-react";
 
 export default defineConfig(() => ({
   root: __dirname,
@@ -13,7 +13,7 @@ export default defineConfig(() => ({
     port: 4300,
     host: 'localhost',
   },
-  plugins: [reactOxc()],
+  plugins: [react()],
   // Uncomment this if you are using workers.
   // worker: {
   //  plugins: [ nxViteTsPaths() ],
