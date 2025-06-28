b34b174be27d9378c875dd4afeae4e05ccf3a93c
lastsunday <lastsunday@yeah.net>
2025-06-28 15:10:26 +0800

    chore: update dependencies

--- apps/server-ui/vite.config.ts
+++ apps/server-ui/vite.config.ts
@@ -1,6 +1,6 @@
 /// <reference types='vitest' />
 import { defineConfig } from 'vite';
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-oxc";
 import { resolve } from 'path';
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import UnoCSS from 'unocss/vite'
