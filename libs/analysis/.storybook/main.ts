490164decbb77c1085c1b65623b79524272a21aa
lastsunday <lastsunday@yeah.net>
2026-01-20 23:49:08 +0800

    chore: update dependencies for nx

--- libs/analysis/.storybook/main.ts
+++ libs/analysis/.storybook/main.ts
@@ -1,13 +1,15 @@
+import { fileURLToPath } from "node:url";
+import { dirname } from "node:path";
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   stories: ['../src/lib/**/*.@(mdx|stories.@(js|jsx|ts|tsx))'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-interactions'],
+  addons: [getAbsolutePath("@storybook/addon-docs")],
   typescript: {
     reactDocgen: 'react-docgen-typescript',
   },
   framework: {
-    name: '@storybook/react-vite',
+    name: getAbsolutePath("@storybook/react-vite"),
     options: {
       builder: {
         viteConfigPath: 'vite.config.ts',
@@ -21,3 +23,7 @@ export default config;
 // To customize your Vite configuration you can use the viteFinal field.
 // Check https://storybook.js.org/docs/react/builders/vite#configuration
 // and https://nx.dev/recipes/storybook/custom-builder-configs
+
+function getAbsolutePath(value: string): any {
+  return dirname(fileURLToPath(import.meta.resolve(`${value}/package.json`)));
+}
