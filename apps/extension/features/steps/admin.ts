2cf970f3fc5fbb473bb29e6fe144d52e35944aab
lastsunday <lastsunday@yeah.net>
2026-06-06 22:56:33 +0800

    test(server): fix extension e2e test error from serviceworker timeout

--- apps/extension/features/steps/admin.ts
+++ apps/extension/features/steps/admin.ts
@@ -4,14 +4,14 @@ import { expect } from "@playwright/test";
 import { APP_ID } from "@/common/config";
 
 Given('安装了插件的浏览器', async ({ extensionId }) => {
-    await expect(extensionId).toEqual(APP_ID)
+  expect(extensionId).toEqual(APP_ID)
 });
 
 When('打开插件后台管理页面', async ({ page, extensionId }) => {
-    await openAdmin(page, extensionId);
+  await openAdmin(page, extensionId);
 });
 
 Then('待业者应该能看到管理页面的展示', async ({ page }) => {
-    const { checkLayoutDisplay } = await getAdmin(page);
-    await checkLayoutDisplay();
+  const { checkLayoutDisplay } = await getAdmin(page);
+  await checkLayoutDisplay();
 });
